371364a639c6ead466beb41f71869925f47278a7
Eric Arndt <earndt1701@gmail.com>
2026-04-02 03:29:29 -0700

    Headless mode

--- tests/test_SmokeTest.py
+++ tests/test_SmokeTest.py
@@ -3,6 +3,7 @@ import pytest
 import time
 import json
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
@@ -12,7 +13,12 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
   def teardown_method(self, method):
